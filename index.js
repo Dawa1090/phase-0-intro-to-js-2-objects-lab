4a17cbc471066208a691d2fabcf99f0dd04653b8
Dawa Sherpa <dawasherpa1090@gmail.com>
2023-07-04 18:24:04 -0400

    object

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name: "dawa",
+    address: "123 street"
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const duplicateEmployee = { ...employee };
+    duplicateEmployee[key] = value;
+    return duplicateEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]=value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee , key ) {
+    const duplicateEmployee = { ...employee };
+    delete duplicateEmployee[key];
+    return duplicateEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+     delete employee[key];
+     return employee;
+}
